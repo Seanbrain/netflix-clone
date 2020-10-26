b8819e958cd5be28c05f8cf786430b1844e6ff2c
Victor-Brain Edeghonghon Esebanmen <seanbrain84@gmail.com>
2020-10-26 18:26:17 +0100

    firebase hosting configurations added

--- src/Nav.js
+++ src/Nav.js
@@ -17,7 +17,7 @@ function Nav() {
 
             <img className="nav__avatar"  
             src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt="Avatar" />
-            _images/1240"
+            
         </div>
     )
 }
